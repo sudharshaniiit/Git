9973bcf5128f5b751b0a157357bb4fdf0c397c97
sudharshan <sudharshaniiit@gmail.com>
2020-09-10 02:11:15 +0000

    added new line

--- Helloworld.java
+++ Helloworld.java
@@ -4,6 +4,7 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, Goodmorning"); 
+        System.out.println("Hello, Goodmorning");
+	System.out.println("Hello, Good"); 
     } 
 }
